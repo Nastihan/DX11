1af8787f0ea2ada9268e4a172f93148de342b7ca
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-04-30 21:57:42 +0330

    add message loop (functioning window)

--- DX11/WInMain.cpp
+++ DX11/WInMain.cpp
@@ -1,5 +1,17 @@
 #include <Windows.h>
 
+
+LRESULT CALLBACK WindProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(100);
+		break;
+	}
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
 	HINSTANCE hPrevInstance,
@@ -13,7 +25,7 @@ int CALLBACK WinMain(
 	WNDCLASSEX wc = { 0 };
 	wc.cbSize = sizeof(wc);
 	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = DefWindowProc;
+	wc.lpfnWndProc = WindProc;
 	wc.cbClsExtra = 0;
 	wc.cbWndExtra = 0;
 	wc.hInstance = hInstance;
@@ -36,6 +48,24 @@ int CALLBACK WinMain(
 	// show the window
 	ShowWindow(hWnd, SW_SHOW);
 
-	while (true);
+	// message
+	MSG msg;
+	BOOL gResult;
+	while (gResult = GetMessage(&msg, nullptr, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	if (gResult == -1)
+	{
+		return -1;
+	}
+	else
+	{
+		return msg.wParam;
+	}
+
+	
 	return 0;
 }
