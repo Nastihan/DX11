707e08d0631839a9ddc66c7b1e76ef3a3b68aa05
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-05-01 21:45:20 +0330

    encapsulate all the functionality of windows in a simple window class as an interface

--- DX11/WInMain.cpp
+++ DX11/WInMain.cpp
@@ -1,16 +1,6 @@
-#include <Windows.h>
+#include "Window.h"
 
 
-LRESULT CALLBACK WindProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(100);
-		break;
-	}
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-}
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -19,34 +9,7 @@ int CALLBACK WinMain(
 	int       nCmdShow)
 {
 	
-	const LPCWSTR pClassName = L"hw3d";
-	
-	// register
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WindProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = pClassName;
-	wc.hIcon = nullptr;
-	RegisterClassEx(&wc);
-
-	// instance
-	HWND hWnd = CreateWindowExW(
-		0, pClassName, L"hw3d Window",
-		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
-		200, 200, 650, 480,
-		nullptr, nullptr, hInstance, nullptr
-	);
-
-	// show the window
-	ShowWindow(hWnd, SW_SHOW);
+	Window wnd(800, 300, L"DX11");
 
 	// message
 	MSG msg;
---
+++ DX11/Window.h
@@ -0,0 +1,35 @@
+#pragma once
+#include "NastihanWin.h"
+
+class Window
+{
+private:
+
+	class WindowClass
+	{
+	public:
+		static const LPCWSTR GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const WindowClass&) = delete;
+		static constexpr const LPCWSTR wndClassName = L"Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInst;
+	};
+public:
+	Window(int width, int height, const LPCWSTR name) noexcept;
+	~Window();
+	Window(const Window&) = delete;
+	Window& operator=(const Window&) = delete;
+private:
+	static LRESULT CALLBACK HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	static LRESULT CALLBACK HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+	LRESULT HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept;
+private:
+	int width;
+	int height;
+	HWND hWnd;
+};
