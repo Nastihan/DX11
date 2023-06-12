f0532a2643dac64660ee41b0e08d2bb8ca981309
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-06-12 20:59:14 +0330

    implement bindablecodex class (a singleton class containing an unordered_map which hashes from a unique id for each bindable to the bindable object )

---
+++ DX11/BindableCodex.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <unordered_map>
+#include <string>
+#include <memory>
+#include "Drawable.h"
+
+namespace Bind
+{
+	class Codex
+	{
+		template<class T,typename...Params>
+		static std::shared_ptr<T> Resolve(Graphics& gfx,Params&&...p) noxnd
+		{
+			return Get().Resolve_<T>(gfx, std::forward<Params>(p)...);
+		}
+	private:
+		template<class T, typename...Params>
+		std::shared_ptr<T> Resolve_(Graphics& gfx, Params&&...p) noxnd
+		{
+			const auto key = T::GenerateUID(std::forward<Params>(p)...);
+			const auto i = binds.find(key);
+			if (i == binds.end())
+			{
+				auto bind = std::make_shared<T>(gfx, std::forward<Params>(p)...);
+				binds[key] = bind;
+				return bind;
+			}
+			else
+			{
+				return std::static_pointer_cast<T>(i->second);
+			}
+		}
+	private:
+		static Codex& Get()
+		{
+			static Codex codex;
+			return codex;
+		}
+	private:
+		std::unordered_map <std::string, std::shared_ptr<Bindable>> binds;
+	};
+}
