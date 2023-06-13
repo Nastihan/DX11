0320412e48008b17e994758e3869d9bdfc8e313a
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-06-14 01:23:44 +0330

    implement bindable transformcbuf for use in the pixel shader

--- DX11/TransformCbuf.h
+++ DX11/TransformCbuf.h
@@ -7,7 +7,7 @@ namespace Bind
 {
 	class TransformCbuf : public Bindable
 	{
-	private:
+	protected:
 		struct Transforms
 		{
 			DirectX::XMMATRIX modelView;
@@ -16,6 +16,9 @@ namespace Bind
 	public:
 		TransformCbuf(Graphics& gfx, const Drawable& parent, UINT slot = 0u);
 		void Bind(Graphics& gfx) noexcept override;
+	protected:
+		void UpdateBindImpl(Graphics& gfx, const Transforms& tf) noexcept;
+		Transforms GetTransforms(Graphics& gfx) noexcept;
 	private:
 		static std::unique_ptr<VertexConstantBuffer<Transforms>> pVcbuf;
 		const Drawable& parent;
---
+++ DX11/TransformCbufPS.cpp
@@ -0,0 +1,29 @@
+#include "TransformCbufPS.h"
+
+namespace Bind
+{
+	TransformCbufPS::TransformCbufPS(Graphics& gfx, const Drawable& parent, UINT slotV, UINT slotP)
+		:
+		TransformCbuf(gfx, parent, slotV)
+	{
+		if (!pPcbuf)
+		{
+			pPcbuf = std::make_unique<PixelConstantBuffer<Transforms>>(gfx, slotP);
+		}
+	}
+
+	void Bind::TransformCbufPS::Bind(Graphics& gfx) noexcept
+	{
+		const auto tf = GetTransforms(gfx);
+		TransformCbuf::UpdateBindImpl(gfx, tf);
+		UpdateBindImpl(gfx, tf);
+	}
+
+	void TransformCbufPS::UpdateBindImpl(Graphics& gfx, const Transforms& tf) noexcept
+	{
+		pPcbuf->Update(gfx, tf);
+		pPcbuf->Bind(gfx);
+	}
+
+	std::unique_ptr<PixelConstantBuffer<TransformCbuf::Transforms>> TransformCbufPS::pPcbuf;
+}
