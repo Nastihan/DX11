fc12234e318a416cfb3219eb53810bd5125b7127
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-07 16:49:46 +0330

    fixed blur effect pixel shader. resolve two issues. - the pixelstep delta was calculated with integer division producing  the result '0' making the output identical to the texture.  --  you cannot directly assign values to a float2 variable using parentheses and comma-separated values, if you do so the comma operator takes action and discards all values before itself, and just assigns the last expression to the variable, instead you have to use the float2 constructor explicitly.

--- DX11/Blur_PS.hlsl
+++ DX11/Blur_PS.hlsl
@@ -14,18 +14,17 @@ float4 main(PS_Input input) : SV_TARGET
     uint width, height;
     tex.GetDimensions(width,height);
     
-    float pixelStepX = 1 / width;
-    float pixelStepY = 1 / height;
+    float pixelStepX = 1.0f / width;
+    float pixelStepY = 1.0f / height;
     
    
-    float4 acc = { 0.0f, 0.0f, 0.0f, 0.0f };
+    float4 acc = (0.0f, 0.0f, 0.0f, 0.0f);
     
     for (int y = -kernelRadius; y <= kernelRadius; y++)
     {
         for (int x = -kernelRadius; x <= kernelRadius; x++)
         {
-            
-            float2 tc = (input.uv.x + (pixelStepX * x), input.uv.y + (pixelStepY * y));
+            float2 tc = float2 (input.uv.x + (pixelStepX * x), input.uv.y + (pixelStepY * y));
             float4 result = tex.Sample(splr, tc).rgba;
             
             acc += result;
