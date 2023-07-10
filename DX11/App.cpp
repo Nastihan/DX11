5063f2657b92ef55388bc1e3aac07a08eba4c0aa
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-11 01:58:52 +0330

    control blur kernel with imgui

--- DX11/App.cpp
+++ DX11/App.cpp
@@ -114,6 +114,7 @@ void App::DoFrame(float dt)
 	cube.SpawnControlWindow(wnd.Gfx(), "Cube 1");
 	cube2.SpawnControlWindow(wnd.Gfx(), "Cube 2");
 	ShowFPSWindow();
+	rg.RenderWidgets(wnd.Gfx());
 
 	// present
 	wnd.Gfx().EndFrame();
